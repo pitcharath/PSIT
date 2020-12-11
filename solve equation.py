a0c0856e18c248f9f67137a6299e18d247c95531
Ktizutyuu <74358766+Ktizutyuu@users.noreply.github.com>
2020-12-11 17:43:33 +0700

    Add files via upload

---
+++ solve equation.py
@@ -0,0 +1,19 @@
+from sympy import symbols, solve, sympify
+eq = str(input())
+eq2 = eq.replace('x','0')
+eq3 = eq2.replace('0','y')
+#หา y จากสมการข้างบน
+
+sympy_eq = sympify("Eq(" + eq.replace("=", ",") + ")")
+sol = solve(sympy_eq)
+#อันนี้หา x ได้แล้ว
+sympy_eq2 = sympify("Eq(" + eq3.replace("=", ",") + ")")
+sol2 = solve(sympy_eq2)
+find_y = sol2[0]*(-1)
+#หา y
+
+box_x = [sol,0]
+box_y = [0]
+box_y.append(find_y)
+print(find_y)
+print(sol)
